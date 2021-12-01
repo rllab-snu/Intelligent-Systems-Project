dd656645ce6ff388846dd4e68647a44140b8ace4
jeongwoo <ohjw420@gmail.com>
2021-12-01 19:59:06 +0900

    project3 skeleton code released

---
+++ sim2real/project/IS_RLLAB/project/RLLAB_project3.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+
+
+##### add python path #####
+import sys
+import os
+import rospkg
+import rospy
+
+PATH = rospkg.RosPack().get_path("sim2real") + "/scripts"
+print(PATH)
+sys.path.append(PATH)
+
+
+import gym
+import env
+import numpy as np
+from collections import deque
+import json
+import random
+import math
+import yaml
+import time
+from sim2real.msg import Result, Query
+from joblib import dump, load
+
+
+# please using sklearn for gaussian process
+# please using tensorflow1 for implementing deep learning architecture
+# torch and different version tensorflow are not allowed.
+from sklearn.utils import shuffle
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+import tensorflow as tf
+print(tf.__version__) # 1.9.0 -> if error occurs, $ pip2 install protobuf==3.7
+
+
+project_path = rospkg.RosPack().get_path("sim2real")
+yaml_file = project_path + "/config/eval.yaml"
+
+TEAM_NAME = "RLLAB"
+team_path = project_path + "/project/IS_" + TEAM_NAME
+
+class Agent:
+    def __init__(self, args):
+        rospy.init_node('agent_' + TEAM_NAME, anonymous=True, disable_signals=True)
+        # env reset with world file
+        self.env = gym.make('RCCar-v0')
+        self.env.seed(1)
+        self.env.unwrapped
+        self.env.load(args['world_name'])
+        self.track_list = self.env.track_list
+        
+        self.time_limit = 100.0
+        
+        ########################
+        # PLEASE READ COMMENTS #
+        ########################
+        """
+        Set some class variables and action parameters.
+        Also, initialize some functions before start if you need.
+        if you trained model using some learning-based methods like as gaussian process or deep learning,
+        please load the pretrained model in this part!!
+        Also, check again your git repository on github website for correctness of your model file push history.
+        """
+        # Please satisfying your clipped velocity and angular velocity
+        # 1.5 <= minVel <= maxVel <= 3.0
+        # 0.0 <= maxAng <= 1.5
+        self.maxAng = 1.0
+        self.minVel = 1.5
+        self.maxVel = 3.0
+        
+
+        self.query_sub = rospy.Subscriber("/query", Query, self.callback_query)
+        self.rt_pub = rospy.Publisher("/result", Result, queue_size = 1)
+
+        print("completed initialization")
+
+
+    def get_action(self, obs):
+        """
+        1) input observation is the raw data from environment.
+        2) 0 to 1080 components (totally 1081) are from lidar.
+           Also, 1081 component and 1082 component are scaled velocity and steering angle, respectively.
+        3) calculate appropriate actions using your method.
+        """
+
+    def callback_query(self, data):
+        rt = Result()
+        START_TIME = time.time()
+        is_exit = data.exit
+        try:
+            # if query is valid, start
+            if data.name != TEAM_NAME:
+                return
+            
+            if data.world not in self.track_list:
+                END_TIME = time.time()
+                rt.id = data.id
+                rt.trial = data.trial
+                rt.team = data.name
+                rt.world = data.world
+                rt.elapsed_time = END_TIME - START_TIME
+                rt.waypoints = 0
+                rt.n_waypoints = 20
+                rt.success = False
+                rt.fail_type = "Invalid Track"
+                self.rt_pub.publish(rt)
+                return
+            
+            print("[%s] START TO EVALUATE! MAP NAME: %s" %(data.name, data.world))
+            obs = self.env.reset(name = data.world)
+            obs = np.reshape(obs, [1,-1])
+            
+            
+            while True:
+                if time.time() - START_TIME > self.time_limit:
+                    END_TIME = time.time()
+                    rt.id = data.id
+                    rt.trial = data.trial
+                    rt.team = data.name
+                    rt.world = data.world
+                    rt.elapsed_time = END_TIME - START_TIME
+                    rt.waypoints = self.env.next_checkpoint
+                    rt.n_waypoints = 20
+                    rt.success = False
+                    rt.fail_type = "Exceed Time Limit"
+                    self.rt_pub.publish(rt)
+                    print("EXCEED TIME LIMIT")
+                    break
+                
+                act = self.get_action(obs)
+                input_steering = np.clip(act[0][0], -self.maxAng, self.maxAng)
+                input_velocity = np.clip(act[0][1], self.minVel, self.maxVel)
+                obs, _, done, logs = self.env.step([input_steering, input_velocity])
+                obs = np.reshape(obs, [1,-1])
+                
+                if done:
+                    END_TIME = time.time()
+                    rt.id = data.id
+                    rt.trial = data.trial
+                    rt.team = data.name
+                    rt.world = data.world
+                    rt.elapsed_time = END_TIME - START_TIME
+                    rt.waypoints = logs['checkpoints']
+                    rt.n_waypoints = 20
+                    rt.success = True if logs['info'] == 3 else False
+                    rt.fail_type = ""
+                    print(logs)
+                    if logs['info'] == 1:
+                        rt.fail_type = "Collision"
+                    if logs['info'] == 2:
+                        rt.fail_type = "Exceed Time Limit"
+                    self.rt_pub.publish(rt)
+                    print("publish result")
+                    break
+        
+        except Exception as e:
+            print(e)
+            END_TIME = time.time()
+            rt.id = data.id
+            rt.trial = data.trial
+            rt.team = data.name
+            rt.world = data.world
+            rt.elapsed_time = END_TIME - START_TIME
+            rt.waypoints = 0
+            rt.n_waypoints = 20
+            rt.success = False
+            rt.fail_type = "Script Error"
+            self.rt_pub.publish(rt)
+
+        if is_exit:
+            rospy.signal_shutdown("End query")
+        
+        return
+
+if __name__ == '__main__':
+    with open(yaml_file) as file:
+        args = yaml.load(file)
+    Agent(args)
+    rospy.spin()
+
